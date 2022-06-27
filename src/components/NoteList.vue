2d37e1ef0dca8cff08e04ac6413fa3d163a07741
Melih Can <melihcankilic@gmail.com>
2022-06-27 22:16:23 +0300

    tablo düzenleme

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,90 +1,40 @@
 <template>
     <q-btn @click="listNotes">List Notes</q-btn>
-    <br><br>
-    <!--q-table
-      title="Note List"
-      :rows="notes"
-      :columns="[{name:'noteId',label:'Note Id',field:'noteId'},{label:'Title',field:'title'},{label:'Description',field:'description'},{label:'Content',field:'content'},{label:'Update',field:''}]"
-      row-key="name"
-      dark
-      color="amber"
-      v-if="notes.length > 0"
-    >
-    </q-table-->
-
-    <!--q-table color="amber">
-      <q-tr><q-th>a</q-th><q-th>b</q-th></q-tr>
-      <q-tr><q-td>c</q-td><q-td>d</q-td></q-tr>
-      <q-tr><q-td>e</q-td><q-td>f</q-td></q-tr>
-      <q-tr><q-td>e</q-td></q-tr>
-    </q-table-->
     <div class="q-pa-md">
     <q-table
-    :rows="notes"
-    :columns="Columns"
-    row-key="id"
-    v-if="notes.length > 0"
+      title="Treats"
+      :rows="notes"
+      :columns="Columns"
+      row-key="name"
+      :selected-rows-label="getSelectedString"
+      selection="multiple"
+      v-model:selected="selected"
+      v-if="notes.length > 0"
     >
-    <!--template #body-cell="notes">
-      <q-td><q-btn></q-btn></q-td>
-    </template-->
-    <!--template #body-cell="props">
-      <q-td
-        :props="props"
-      >
-        <q-btn
-          flat
-          color="primary"
-          :label="props.value"
-          @click="listNotes"
-        />
-      </q-td>
-    </template-->
-    <!--template v-slot:body-cell-actions="props">
-            <q-td :props="props">
-              <q-btn dense round flat color="grey" @click="listNotes" icon="edit"></q-btn>
-              <q-btn dense round flat color="grey" @click="listNotes" icon="delete"></q-btn>
-            </q-td>
-          </template-->
-    <!--template v-slot:header-selection="scope">
-        <q-toggle v-model="scope.selected" />
-      </template-->
 
-      <!--template v-slot:body-selection="scope">
-        <q-toggle v-model="scope.selected" />
-      </template-->
-      <template #body="props">
-      <q-td :props="props.rows"></q-td>
-      </template>
-  </q-table>
-</div>
-    <!--table v-if="notes.length > 0">
-      <tr>
-        <th>noteId</th>
-        <th>title</th>
-        <th>description</th>
-        <th>note</th>
-        <th>update</th>
-      </tr>
-      <tr v-for="(note, index) in notes" :key="index">
-        <td>{{ note.noteId }}</td>
-        <td>{{ note.title }}</td>
-        <td>{{ note.description }}</td>
-        <td>{{ note.content }}</td>
-        <q-btn @click="noteSelected(index)" style="width: 100px;">Select</q-btn>
-      </tr>
-    </table-->
+    <template #body-cell-update="props">
+      <q-td :props="props">
+        <q-btn dense round flat color="grey" @click="listNotes" icon="edit"></q-btn>
+      </q-td>
+    </template>
+
+    </q-table>
+    <div class="q-mt-md">
+      Selected: {{ JSON.stringify(selected) }}
+    </div>
+  </div>
 </template>
 
 <script>
 import noteService from './NoteService.js'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'NoteList',
   setup () {
     const { getAllNotes, noteItemJson, notes } = noteService()
     const Columns = [{ name: 'noteId', label: 'Note Id', field: 'noteId' }, { label: 'Title', field: 'title' }, { label: 'Description', field: 'description' }, { label: 'Content', field: 'content' }, { label: 'Update', field: '' }]
+    const selected = ref([])
     return {
       notes,
       noteItemJson,
@@ -102,6 +52,10 @@ export default defineComponent({
         noteItemJson.title = notes.value[idx].title
         noteItemJson.description = notes.value[idx].description
         noteItemJson.content = notes.value[idx].content
+      },
+      selected,
+      getSelectedString () {
+        return selected.value.length === 0 ? '' : `${selected.value.length} record${selected.value.length > 1 ? 's' : ''} selected of ${notes.length}`
       }
     }
   }
--- src/pages/NotePage.vue
+++ src/pages/NotePage.vue
@@ -4,8 +4,6 @@
     <q-layout class="flex flex-center"-->
       <NoteItemVue></NoteItemVue>
       <NoteListVue></NoteListVue>
-      <MultiVue></MultiVue>
-      <MultiYedekVue></MultiYedekVue>
     <!--/q-layout>
   </q-page-->
 </template>
@@ -14,16 +12,12 @@
 import { defineComponent } from 'vue'
 import NoteItemVue from 'src/components/NoteItem.vue'
 import NoteListVue from 'src/components/NoteList.vue'
-import MultiVue from 'src/components/Multi.vue'
-import MultiYedekVue from 'src/components/MultiYedek.vue'
 export default defineComponent({
   name: 'NotePage',
 
   components: {
     NoteItemVue,
-    NoteListVue,
-    MultiVue,
-    MultiYedekVue
+    NoteListVue
   },
 
   setup () {
